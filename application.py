bf776f9906b2d4f3cb000798cb67f085db42592a
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2022-07-26 21:03:17 -0700

    added detail page

--- application.py
+++ application.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('home.html')
+
+
+@app.route("/detail")
+def detail():
+    return render_template('detail.html')
